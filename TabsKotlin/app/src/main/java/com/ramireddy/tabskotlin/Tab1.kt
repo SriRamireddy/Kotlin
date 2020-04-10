de6de4cc41fd3d05553a8eadedebf495bf857a17
Ramireddy Reddymallu <ramireddy469@gmail.com>
2020-04-10 21:42:11 +0530

    seond commit

--- TabsKotlin/app/src/main/java/com/ramireddy/tabskotlin/Tab1.kt
+++ TabsKotlin/app/src/main/java/com/ramireddy/tabskotlin/Tab1.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +19,17 @@ class Tab1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false)
+        val v : View = inflater.inflate(R.layout.fragment_tab1, container, false)
+        val btn : Button = v.findViewById(R.id.call)
+
+        btn.setOnClickListener { makecall() }
+
+        return v
+    }
+
+    private fun makecall() {
+        val toast : Toast = Toast.makeText(activity,"hello",Toast.LENGTH_LONG)
+        toast.show()
     }
 
 
