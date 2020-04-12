ee4fbe00ac1445bf1676f2211d71fad909cae532
Ramireddy Reddymallu <ramireddy469@gmail.com>
2020-04-12 22:07:40 +0530

    initial commit

---
+++ KotlinNavigationDrawer/app/src/main/java/com/ramireddy/kotlinnavigationdrawer/SampleFragForNaviga.kt
@@ -0,0 +1,24 @@
+package com.ramireddy.kotlinnavigationdrawer
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SampleFragForNaviga : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample_frag_for_naviga, container, false)
+    }
+
+
+}
